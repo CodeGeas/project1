43ba1f610666e9112a505c04b767713b17db8ac5
Ravi KC <ravikc18@gmail.com>
2020-01-05 22:29:04 -0500

    first commit

---
+++ src/main/java/com/codegeas/project1/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.codegeas.project1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration  
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override  
+    public void configure(HttpSecurity http) throws Exception {  
+		http  
+	    .authorizeRequests()  
+	        .antMatchers("/").permitAll()
+	        .antMatchers( "/css/**").permitAll()  
+	        .anyRequest().authenticated()  
+        .and()  
+	        .formLogin()  
+	            .loginPage("/login") 
+	            // if the authentication failed, it will stay in the same login page...
+	            // .. and add "?error" in its url
+	            .loginProcessingUrl("/authenticateTheUser")  
+	            .permitAll()
+         .and()
+	         .logout()
+	         // if the logout succeed, it will redirect to index-page...
+	         // ...and add "?logout" in its url
+		         .logoutSuccessUrl("/?logout")
+		         .permitAll();  
+    }  
+	
+    @Override  
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {  
+        auth.inMemoryAuthentication()  
+            .withUser("user")  
+            .password("{noop}pass") // Spring Security 5 requires specifying the password storage format  
+            .roles("USER");  
+    }  
+}
---
+++ src/main/java/com/codegeas/project1/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.codegeas.project1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+ * @Controller is used to mark classes as Spring MVC Controller. 
+ * @RestController is a convenience annotation that does nothing more than...
+ * ...adding the @Controller and @ResponseBody annotations. 
+ * Also @RestController will treat return value of the methods as String only...
+ * ...so it will not render respective html in browser
+ */
+@Controller
+public class LoginController {
+	
+   @GetMapping("/")
+    public String rootPage() {  
+        return "index";  
+    } 
+   
+    @GetMapping("/login")  
+    public String login() {  
+        return "login-form";  
+    }  
+    
+	@GetMapping("/home-page")
+	public String homePage() {
+		return "home-page";
+	}
+}
